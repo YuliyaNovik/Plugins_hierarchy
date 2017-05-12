ba3e2a71ef939c4445ce77e5e00da1813316b797
Yuliya Novik <Yuliya Novik>
2017-05-12 10:03:54 +0300

    Add lists of plugins, types and objects

--- Class/Model.cs
+++ Class/Model.cs
@@ -6,5 +6,9 @@ using System.Threading.Tasks;
 
 namespace Plugins_hierarchy.Class {
     public static class Model {
+        public static List<Plugin> pluginList { get; set; } = new List<Plugin>();
+        public static List<Object> typeList { get; set; } = new List<Object>();
+        public static List<Object> objectList { get; set; } = new List<Object>();
+        public static List<Int32> objectTypeList { get; set; } = new List<Int32>();
     }
 }
