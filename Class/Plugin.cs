31b03153151984e6a8bfa32bf54eee4b92ae6a09
Yuliya Novik <Yuliya Novik>
2017-05-12 09:49:29 +0300

    Add processing dll as plugin

--- Class/Plugin.cs
+++ Class/Plugin.cs
@@ -3,8 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.IO;
 
 namespace Plugins_hierarchy.Class {
     public class Plugin {
+        public string className { get; }
+        public Type objectType { get; }
+        private string namespaceName;
+        private string pluginName;
+        private string pathToDir;
+
+
+        public Plugin(string pathToDir) {
+            string descriptionFile;
+            using (StreamReader sr = new StreamReader(pathToDir + "plugin.plg")) {
+                descriptionFile = sr.ReadToEnd();
+            }
+            string[] parsedFile = descriptionFile.Split(' ');
+
+            this.className = parsedFile[0];
+            this.namespaceName = parsedFile[1];
+            this.pluginName = parsedFile[2];
+
+            Assembly asmDll = Assembly.LoadFrom(pathToDir + pluginName);
+            this.objectType = asmDll.GetType(namespaceName + "." + className);
+        }
+
+        public Object CreateObject() {
+            object obj = Activator.CreateInstance(objectType);
+            return obj;
+        }
+
+        public FieldInfo[] GetFileds() {
+            return this.objectType.GetFields();
+        }
+
+        public PropertyInfo[] GetProperties() {
+            return this.objectType.GetProperties();
+        }
     }
 }
